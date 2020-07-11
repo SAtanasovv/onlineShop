6bab1f5d41a786e5ad34d9c145230618b0526cc3
SAtanasovv <slav.1995@abv.bg>
2020-07-12 00:40:09 +0300

    controller test run

--- src/main/java/com/satanasov/onlineShop/controller/AddressController.java
+++ src/main/java/com/satanasov/onlineShop/controller/AddressController.java
@@ -1,4 +1,18 @@
 package com.satanasov.onlineShop.controller;
 
+import com.satanasov.onlineShop.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/users")
 public class AddressController {
+
+    @PostMapping
+    public String createAddress(@RequestBody User user){
+        return "Hello ".concat(user.getFirstName());
+    }
+    @GetMapping("/test")
+    private String test(){
+        return "Hello";
+    }
 }
